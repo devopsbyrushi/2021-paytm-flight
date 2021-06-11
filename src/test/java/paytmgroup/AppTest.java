7d1d804650a6f611f5df73d4f0ba93a3b5f260e8
Rushi <rushi@mycomapany.com>
2021-06-11 07:58:58 +0530

    dev dev code for flight in paytm

---
+++ src/test/java/paytmgroup/AppTest.java
@@ -0,0 +1,20 @@
+package paytmgroup;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
